317bff9c294c63190b364c8ca1c59ccc9a19c0a7
chenle <chen.le@aliyun.com>
2020-10-27 17:56:41 +0800

    同步

--- code/hb-mobile-statics/src/networks/index.js
+++ code/hb-mobile-statics/src/networks/index.js
@@ -5,16 +5,20 @@ import Vue from "vue";
 import axios from "axios";
 
 let config = {
-  baseURL: 'http://localhost:8090/hbydGame',
-  proxy: {
-    '/api': {
-      target: "http://192.168.1.150:90/api",
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api': ''
-      }
-    }
+  baseURL: 'http://10.61.2.175:8090/hbydGame',
+  headers:{
+    "Access-Control-Allow-Origin":"*",
+    "Access-Control-Max-Age":"3600",
+    "Access-Control-Allow-Headers":"content-type,Authorization",
+    "Access-Control-Allow-Credentials":"true",
+
+
+    /* response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "content-type,Authorization");
+         response.setHeader("Access-Control-Allow-Credentials", "true");*/
   }
+
 };
 const _axios = axios.create(config);
 
@@ -64,6 +68,7 @@ Vue.use(Plugin);
 export default Plugin;
 
 /*http://localhost:8090/hbydGame/role/getRolePermissionMenu 获取菜单栏数据
+/*http://10.61.2.175:8090/hbydGame/role/getRolePermissionMenu 获取菜单栏数据
 *
 *
 *
